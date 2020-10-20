e3e28681f7063c620c91d6b85da38a5bb4313c0d
ka2142las <ka1234las@yandex.ru>
2020-10-20 11:19:39 +0300

    радактирование шапки

--- Laravel_Web/resources/views/home.blade.php
+++ Laravel_Web/resources/views/home.blade.php
@@ -9,18 +9,17 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-    </head>
-<body>
-<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-  <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
-  <nav class="my-2 my-md-0 mr-md-3">
-    <a class="p-2 text-dark" href="#">Features</a>
-    <a class="p-2 text-dark" href="#">Enterprise</a>
-    <a class="p-2 text-dark" href="#">Support</a>
-    <a class="p-2 text-dark" href="#">Pricing</a>
-  </nav>
-  <a class="btn btn-outline-primary" href="#">Sign up</a>
-</div>
+</head>
+
+<body class="bg-dark">
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto text-white font-weight-normal">LARAVEL-template</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-white" href="/">Главная</a>
+            <a class="p-2 text-white" href="/about">Про нас</a>
+        </nav>
+        <a class="btn btn-outline-warning" href="#">Отзывы</a>
+    </div>
 </body>
 
 </html>
